2ae8b4257a38f67b460c62f2f5fc2af1c1619cd0
Marat <remarr@gmail.com>
2020-08-09 01:14:00 +0300

    setup traefik

--- run-deploy.sh
+++ run-deploy.sh
@@ -1,8 +1,3 @@
 git pull
-# docker-compose build --no-cache
-# docker stop nginx-proxy
-# docker-compose down && docker-compose up -d
-# docker network connect blogm4rrdeploy_default nginx-proxy
-# docker start nginx-proxy
 
 docker-compose up --build -d
