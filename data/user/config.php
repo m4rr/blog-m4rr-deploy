27548599412a99eb9a28184761958fea63eb9e40
Marat <remarr@gmail.com>
2020-08-09 02:10:51 +0300

    /

--- data/user/config.php
+++ data/user/config.php
@@ -3,6 +3,6 @@
 // see system/default/config.php for configurable parameters
 
   $_config['url_composition'] = 'synthetic';
-  $_config['preferred_domain_name'] = "https://m4rr.ru/blog/"; /* null or string */
+  $_config['preferred_domain_name'] = "https://m4rr.ru/blog"; /* null or string */
 
 ?>
